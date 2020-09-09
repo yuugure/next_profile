9e9c13836fb1dfc4f16685996d5e8ec273199341
yuugure <westhillworker@gmail.com>
2020-09-09 17:27:34 +0900

    navigation menu add

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,21 @@
+import Link from 'next/link'
+
 const Header = () => (
   <header>
-    <h1 className="text-center">My Profile</h1>
+    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <span className="font-semibold text-xl tracking-tight">My Profile</span>
+      </div>
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow">
+          <Link href="/">
+            <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              Home
+            </a>
+          </Link>
+        </div>
+      </div>
+    </nav>
   </header>
 )
 
